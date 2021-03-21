348015c81ce5a0623b5e41d54034f06247c6b965
nicolas433 <ngrisoste@gmail.com>
2021-03-21 19:14:29 -0300

    Criando função de login e logout

---
+++ src/pages/api/logout.ts
@@ -0,0 +1,5 @@
+import { VercelRequest, VercelResponse } from '@vercel/node'
+
+export default async (request: VercelRequest, response: VercelResponse) => {
+    response.json({ auth: false, token: null });
+}
