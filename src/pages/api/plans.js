69a545022759ff12ad3f5b53a554a421d0c047d1
johnnyferreiradev <johnnyferreiradevweb@gmail.com>
2021-03-24 11:35:24 -0300

    Adjusting remove and update plans methods

--- src/pages/api/plans.js
+++ src/pages/api/plans.js
@@ -7,8 +7,6 @@ export default async (request, response) => {
   const auth = verifyJWT(request);
   const collectionName = 'plans';
 
-  console.log('Auth value = ', auth);
-
   if (!auth) {
     return response.status(401).json({
       code: 'Unauthorized',
@@ -27,20 +25,20 @@ export default async (request, response) => {
           name,
           price,
           size,
-          subscribedAt: new Date(),
+          createdAt: new Date(),
         });
 
-        const { _id, subscribedAt } = result;
+        const { _id, createdAt } = result;
 
         response.status(200).json({
           id: _id,
           name,
           price,
           size,
-          subscribedAt,
+          createdAt,
         });
       } catch (e) {
-        response.status(400).json({
+        response.status(500).json({
           error: {
             code: 'plansStore',
             message: 'Erro ao cadastrar um planos',
@@ -57,7 +55,7 @@ export default async (request, response) => {
             return response.status(500).json({
               error: {
                 code: 'plansList',
-                message: 'Erro ao listar planos',
+                message: err,
               },
             });
           }
@@ -65,50 +63,13 @@ export default async (request, response) => {
           response.status(200).json({ plans: result });
         });
       } catch {
-        response.status(400).json({
+        response.status(500).json({
           error: {
             code: 'plansList',
             message: 'Erro ao listar planos',
           },
         });
       }
-      break;
-    }
-
-    case 'DESTROY': {
-      response.status(200).json({ ok: true });
-      // try {
-      //   if (auth === true) {
-      //     const db = await connectToDatabase(process.env.MONGODB_URI);
-      //     db.plans.deleteOne({ id: request.id });
-      //     response.status(200).json({ ok: true });
-      //   } else {
-      //     response = auth;
-      //   }
-      // } catch {
-      //   response.status(400).json({ ok: false })
-      // }
-      break;
-    }
-
-    case 'UPDATE': {
-      response.status(200).json({ ok: true });
-      // try {
-      //   if (auth === true) {
-      //     const { name = " ", price, size } = request.body;
-      //     const db = await connectToDatabase(process.env.MONGODB_URI);
-      //     db.plans.updateOne(({
-      //       name,
-      //       price,
-      //       size,
-      //     }));
-      //     response.status(200).json({ ok: true });
-      //   } else {
-      //     response = auth;
-      //   }
-      // } catch {
-      //   response.status(400).json({ ok: false });
-      // }
       break;
     }
 
