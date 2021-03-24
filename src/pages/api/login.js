416a3c6a9329b2b5753d5bb61968d215897d6b6a
Johnny Ferreira <46564172+johnnyferreiradev@users.noreply.github.com>
2021-03-24 11:39:56 -0300

    Backend (#2)
    * Rota de subscrição no site e primeira conexão com banco.
    
    * Criando função de login e logout
    
    * Criando função de autorização de usuário
    
    * Crud de planos
    
    * Exemplo de .env
    
    * .env ao gitignore
    
    * Novas dependencias
    
    * Ench/backend (#1)
    
    * Adjusting api files
    
    * Adjusting user registration function
    
    * Adjusting user login function
    
    * Refactoring token verification
    
    * Criando cadastro e listagem de planos
    
    * Adjusting remove and update plans methods
    
    Co-authored-by: nicolas433 <ngrisoste@gmail.com>

---
+++ src/pages/api/login.js
@@ -0,0 +1,63 @@
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+
+import connectToDatabase from './connect';
+
+export default async (request, response) => {
+  const { method } = request;
+
+  try {
+    const db = await connectToDatabase(process.env.MONGODB_URI);
+
+    switch (method) {
+      case 'GET': {
+        const { email, password } = request.body;
+
+        const collection = db.collection('user');
+
+        const user = await collection.findOne({ email });
+
+        if (user) {
+          const match = await bcrypt.compare(password, user.password);
+
+          if (match) {
+            const { _id, name, subscribedAt } = user;
+            const token = jwt.sign({ _id }, process.env.SECRET, {
+              expiresIn: '30d',
+            });
+
+            response.status(200).json({
+              user: {
+                id: _id,
+                name,
+                email,
+                subscribedAt,
+              },
+              token,
+            });
+          } else {
+            response.status(400).json({
+              code: 'password',
+              message: 'Senha incorreta',
+            });
+          }
+        } else {
+          response.status(400).json({
+            code: 'email',
+            message: 'Email incorreto',
+          });
+        }
+
+        break;
+      }
+      default: {
+        response.status(405).end(`Method ${method} Not Allowed`);
+      }
+    }
+  } catch (e) {
+    return response.status(400).json({
+      code: 'login',
+      message: 'Erro ao fazer login',
+    });
+  }
+};
